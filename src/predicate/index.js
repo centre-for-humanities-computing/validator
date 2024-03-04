32870bdbd412df78af8e183bf8bafc42dfb75b15
donbjarkone <44224869+donbjarkone@users.noreply.github.com>
2024-03-04 16:43:08 +0100

    added aggregated predicate functions for checking multiple conditions

--- src/predicate/index.js
+++ src/predicate/index.js
@@ -1,9 +1,9 @@
 import { stringTrim } from "../transform/index.js";
-import { Validator } from "../validator.js";
 
 /**
- *
- * @param {Validator} str
+ * A predicate function to check multiple conditions for strings: type and non-emptiness
+ * @param {Validator} str - The Validator instance containing the context to check
+ * @returns {boolean} - `true` if the string passed the tests, otherwise `false`
  */
 export function stringTrimmedNotEmpty(str) {
     return str.fulfillAllOf(str => [
@@ -12,9 +12,50 @@ export function stringTrimmedNotEmpty(str) {
     ]);
 }
 
+/**
+ * A predicate function to check if a number is natural (integer and positive)
+ * @param int - The Validator instance containing the context to check
+ * @return {boolean} - `true` if the number passed the tests, otherwise `false`
+ */
+export function integerGreaterThanZero(int) {
+    return int.fulfillAllOf(int => [
+        int.is.anInteger(),
+        int.is.greaterThan(0)
+    ]);
+}
 
-let test = Validator.createOnErrorThrowValidator();
-test('test', 'name').fulfillAllOf(str => [
-    stringTrimmedNotEmpty,
+/**
+ * A predicate function to check if a number is whole (integer and positive or zero)
+ * @param int - The Validator instance containing the context to check
+ * @return {boolean} - `true` if the number passed the tests, otherwise `false`
+ */
+export function integerGreaterThanEqualZero(int) {
+    return int.fulfillAllOf(int => [
+        int.is.anInteger(),
+        int.is.greaterThanOrEqualTo(0)
+    ]);
+}
 
-], '${PATH} with ${VALUE} must be a non empty string');
+/**
+ * A predicate function to check if a number is positive (greater than zero)
+ * @param num - The Validator instance containing the context to check
+ * @return {boolean} - `true` if the number passed the tests, otherwise `false`
+ */
+export function numberGreaterThanZero(num) {
+    return num.fulfillAllOf(num => [
+        num.is.aNumber(),
+        num.is.greaterThan(0)
+    ]);
+}
+
+/**
+ * A predicate function to check if a number is positive or zero
+ * @param num - The Validator instance containing the context to check
+ * @return {boolean} - `true` if the number passed the tests, otherwise `false`
+ */
+export function numberGreaterThanEqualZero(num) {
+    return num.fulfillAllOf(num => [
+        num.is.aNumber(),
+        num.is.greaterThanOrEqualTo(0)
+    ]);
+}
